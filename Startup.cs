129c4d8f7ffcee4778bcb35f479fd9e5ca1025f7
Victor DiLella <v.dilella@elsevier.com>
2017-06-06 17:48:11 -0400

    Adding dbcontext

--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using vega.Persistence;
 
 namespace WebApplicationBasic
 {
@@ -31,6 +33,7 @@ namespace WebApplicationBasic
         {
             // Add framework services.
             services.AddMvc();
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer("...")); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
