fa6d926692b10a6f34cf8967c4260da1436b8524
Victor DiLella <v.dilella@elsevier.com>
2017-06-06 17:22:20 -0400

    Adding make and model classes

---
+++ Models/Make.cs
@@ -0,0 +1,11 @@
+namespace vega.Models
+{
+    public class Make
+    {
+        public int Id { get; set;}
+
+        public string Name { get; set;}
+
+        public string CountryOfOrigin { get; set;}
+    }
+}
