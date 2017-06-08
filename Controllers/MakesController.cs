7870a0be0ad2893fc71a955deda3d443f3f37ad7
Victor DiLella <DILELLAV@science.regn.net>
2017-06-07 21:30:30 -0400

    Adding make and model resources

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,13 +16,18 @@ namespace vega.Controllers
         public MakesController(VegaDbContext dbContext)
         {
             this.dbContext = dbContext;
-
         }
 
         [HttpGet("/api/makes")]
         public async Task<IEnumerable<Make>> GetMakes()
         {
-            return await dbContext.Makes.Include(m => m.Models).ToListAsync();
+            return await dbContext.Makes.ToListAsync();
+        }
+
+        [HttpGet("/api/make/{id}")]
+        public async Task<IEnumerable<Make>> GetMake(int id)
+        {
+            return await dbContext.Makes.Where(x => x.Id == id).ToListAsync();
         }
     }
 }
---
+++ Controllers/Resources/ModelResource.cs
@@ -0,0 +1,13 @@
+namespace vega.Controllers.Resources
+{
+    public class ModelResource
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Year { get; set; }
+
+        public int MakeId { get; set; }
+    }
+}
