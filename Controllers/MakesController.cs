a9e8507fc164953fa45ea662c65e012001a51783
Victor DiLella <DILELLAV@science.regn.net>
2017-06-07 21:54:02 -0400

    tiny refactor

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using vega.Controllers.Resources;
 using vega.Models;
 using vega.Persistence;
 
@@ -12,26 +13,19 @@ namespace vega.Controllers
 
     public class MakesController : Controller
     {
-        private readonly VegaDbContext dbContext;
-        private readonly IMapper mapper;
+        private readonly VegaDbContext _dbContext;
+        private readonly IMapper _mapper;
 
         public MakesController(VegaDbContext dbContext, IMapper mapper)
         {
-            this.mapper = mapper;
-            this.dbContext = dbContext;
+            _mapper = mapper;
+            _dbContext = dbContext;
         }
 
         [HttpGet("/api/makes")]
         public async Task<IEnumerable<Make>> GetMakes()
         {
-            return await dbContext.Makes.ToListAsync();
-        }
-
-        [HttpGet("/api/make/{id}")]
-        public async Task<IEnumerable<MakeResource>> GetMake(int id)
-        {
-            var makes = await dbContext.Makes.Where(x => x.Id == id).ToListAsync();
-            return mapper.Map<List<Make>, List<MakeResource>>(makes);
+            return await _dbContext.Makes.ToListAsync();
         }
     }
 }
